6aa7618dd99cd2809e3435740933ed10f86d9681
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-05-14 19:01:26 +0000

    printed some chunks

---
+++ print-chunks.js
@@ -0,0 +1,16 @@
+var https = require('https');
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+https.get(requestOptions, function (response){
+  response.setEncoding('utf8');
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data);
+  });
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+});
