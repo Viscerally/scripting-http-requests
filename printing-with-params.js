5a00a7880771a0c6f60e6d38c034c7e7b594a64b
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-05-15 03:23:27 +0000

    made more modular

--- printing-with-params.js
+++ printing-with-params.js
@@ -1,10 +1,8 @@
-var https = require('https');
+// var https = require('https');
 
-function getAndPrintHTML (options) {
-var pathArr = ['sytantris.github.io', '/http-examples/step2.html'];
-  var requestOptions = {
-    host: pathArr[0],
-    path: pathArr[1]
+/*function getAndPrintHTML (options) {
+  // var pathArr = ['sytantris.github.io', '/http-examples/step2.html'];
+  
   };
 
 https.get(requestOptions, function (response){
@@ -20,4 +18,33 @@ https.get(requestOptions, function (response){
     });
   });
 }
-getAndPrintHTML(pathArr);
+var requestOptions = {
+  host: ‘sytantris.github.io’,
+  path: ‘/http-examples/step3.html’
+ };
+ getAndPrintHTML(requestOptions); */
+
+ // while https is built-in to Node, it is a module, so it must be required
+var https = require('https');                              
+// the host can be thought of as the domain name you want to read from,
+// and the path is the resource - ‘/’ is the root path, but if you wanted to read a
+// particular resource (like ‘/login/index.html’), that would be defined in the path
+function getAndPrintHTML (options) {
+ https.get(options, function (response) {
+   // set encoding of received data to UTF-8
+   response.setEncoding('utf8');
+   var buf = '';
+   response.on('data',function(chunk){
+     buf += chunk;
+   });
+   response.on('end', function() {
+     console.log('Response stream complete');
+     console.log(buf);
+   });
+ });
+}
+var requestOptions = {
+ host: 'sytantris.github.io',
+ path: '/http-examples/step3.html'
+};
+getAndPrintHTML(requestOptions);
